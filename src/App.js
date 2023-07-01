8b63f4857cadf982c510cc76734be686cde7b904
Mickael <mickael.pedro@outlook.com>
2023-07-01 23:48:01 +0200

    Implemented basic Picture Search functionnality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import ImagesResult from './components/ImagesResult';
+import SearchBar from './components/SearchBar';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [field, setField] = useState("");
+  const [images, setImages] = useState([]);
+
+  function handleFieldChange(e) {
+    setField(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchImages = () => {
+      if (field !== "") {
+        (async () => {
+            await axios.get(`https://pixabay.com/api/?key=17555297-46a99d3dc7abf78679ec9e640&q=${field}&image_type=photo&per_page=6&pretty=true/`).then(res => {
+              setImages(res.data.hits);
+            });
+            console.log(field);
+        })();
+      } else {
+        setImages(null);
+      }
+    };
+
+    const timeout = setTimeout (() => fetchImages(), 500)
+    return () => clearTimeout(timeout);
+  }, [field]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar value={field} onChange={handleFieldChange}/>
+      <ImagesResult images={images}/>
     </div>
   );
 }
