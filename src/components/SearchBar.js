e27e3eb0572f85b065dd0c134f5edd2e0a4de3e8
Mickael <mickael.pedro@outlook.com>
2023-07-01 21:36:57 +0200

    Created SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+export default function SearchBar({value, onChange}) {
+    return (
+        <div class="w-full max-w-xs mt-3 mx-3">
+            <input 
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="query" 
+                type="text"
+                placeholder="Search..."
+                onChange={onChange}
+            />
+        </div>
+    );
+}
